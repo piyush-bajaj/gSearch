598edf4ec35d9a281f788a3a4b76339d0d61b1d9
piyush-bajaj <bajaj.piyush.1995@gmail.com>
2023-07-23 18:47:42 +0000

    some css changes

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -30,35 +30,39 @@ export const Results = () => {
         case "/":
         case "/search":
             return (
-                <div className='flex flex-wrap justify-between space-y-6 sm:px-56 mt-5'>
-                    {
-                        results?.items?.map(({link, title}:searchResult, index:number) => (
-                            <div key={index} className='w-full'>
-                                <a href={link} target='_blank' rel='noreferrer'>
-                                    <p className='text-sm'>
-                                        {link.length > 30 ? link.substring(0,30) : link}
-                                    </p>
-                                    <p className='text-lg hover:underline dark:text-blue-300 text-blue-700'>
-                                        {title}
-                                    </p>
-                                </a>
-                            </div>
-                        ))
-                    }
-                </div>
+                <>
+                    <div className='flex flex-wrap justify-between space-y-6 sm:px-56 mt-5'>
+                        {
+                            results?.items?.map(({link, title}:searchResult, index:number) => (
+                                <div key={index} className='w-full'>
+                                    <a href={link} target='_blank' rel='noreferrer'>
+                                        <p className='text-sm'>
+                                            {link.length > 30 ? link.substring(0,30) : link}
+                                        </p>
+                                        <p className='text-lg hover:underline dark:text-blue-300 text-blue-700'>
+                                            {title}
+                                        </p>
+                                    </a>
+                                </div>
+                            ))
+                        }
+                    </div>
+                </>
             );
         case "/images":
             return (
-                <div className="flex flex-wrap justify-center items-center">
-                    {
-                        results?.items?.map(({ link, title, image }:searchResult, index:number) => (
-                            <a href={image?.contextLink} target="_blank" key={index} rel="noreferrer" className="sm:p-3 p-5">
-                                <img src={link} alt={title} loading="lazy" className='w-36 h-40'/>
-                                <p className="sm:w-36 w-36 break-words text-sm mt-2">{title}</p>
-                            </a>
-                        ))
-                    }
-                </div>
+                <>
+                    <div className="flex flex-wrap justify-center space-y-6 text-center">
+                        {
+                            results?.items?.map(({ link, title, image }:searchResult, index:number) => (
+                                <a href={image?.contextLink} target="_blank" key={index} rel="noreferrer" className="sm:p-3 p-5">
+                                    <img src={link} alt={title} loading="lazy" className='w-36 h-40'/>
+                                    <p className="sm:w-36 w-36 break-words text-sm mt-2">{title}</p>
+                                </a>
+                            ))
+                        }
+                    </div>
+                </>
             )
     
         default:
--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -14,8 +14,8 @@ export const Search = () => {
       }, [debounceValue]);
 
   return (
-    <div className="relative sm:ml-48 md:ml-72 sm:-mt-10 mt-3">
-        <input type='text' value={text} className='w-80 h-10 dark:bg-gray-200 border rounded-full shadow-sm outline-none p-6 text-black hover:shadow-lg sm:w-96' placeholder='Enter something to search' onChange={(e) => setText(e.target.value)}/>
+    <div className="relative sm:-mt-10 mt-3 ml-[10%]">
+        <input type='text' value={text} className='w-full h-10 dark:bg-gray-200 border rounded-full shadow-sm outline-none p-6 text-black hover:shadow-lg ' placeholder='Enter something to search' onChange={(e) => setText(e.target.value)}/>
         {text && (
             <button type='button' className='absolute top-2 right-4 text-2xl text-gray-500' onClick={() => setText('')}>
                 X
